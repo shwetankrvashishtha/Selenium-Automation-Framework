593e814769ea2ab136c709111c17e48984df3b33
chinshah <chinshah@blackberry.com>
2018-02-09 09:46:16 +0530

    Minor changes

--- src/test/java/functionalLib/TestBase.java
+++ src/test/java/functionalLib/TestBase.java
@@ -15,7 +15,6 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
