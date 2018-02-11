8ce7fa8519ce02089f8d52da296a057887d3663e
Shwetank Vashishtha <vashishthashwetank@gmail.com>
2018-02-11 07:58:40 +0530

    Added drivers in pom.xml

--- src/test/java/functionalLib/TestBase.java
+++ src/test/java/functionalLib/TestBase.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
